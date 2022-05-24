fb85590dbd406c85ec7eddf57eb1467c81136726
bonox2 <illiagoncharov99@gmail.com>
2022-05-24 18:04:24 -0400

    movie page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import MoviePageCard from './components/MoviePage';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MoviePageCard/>
+    </>
+  );
+}
+
+export default App;
